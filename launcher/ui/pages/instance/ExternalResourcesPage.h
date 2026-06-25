d868fda73dafa0091c894c64f9e82ed0e5cea602
GreyW0lf1888 <hyx20130723@gmail.com>
2026-06-25 18:47:45 +0800

    Update ExternalResourcesPage.h
    Signed-off-by: GreyW0lf1888 <hyx20130723@gmail.com>

--- launcher/ui/pages/instance/ExternalResourcesPage.h
+++ launcher/ui/pages/instance/ExternalResourcesPage.h
@@ -9,6 +9,7 @@
 #include "ui/pages/BasePage.h"
 
 class ResourceFolderModel;
+class QVBoxLayout; // Forward declaration for your layout pointer
 
 namespace Ui {
 class ExternalResourcesPage;
@@ -75,4 +76,8 @@ class ExternalResourcesPage : public QMainWindow, public BasePage {
     QString m_viewFilter;
 
     std::shared_ptr<Setting> m_wide_bar_setting = nullptr;
+
+private:
+    void createOfflineCosmeticsPanel(QVBoxLayout *mainLayout);
 };
+
